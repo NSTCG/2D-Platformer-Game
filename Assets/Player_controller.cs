e3efe4ad79c7abeeb43d36f4feda189a14f2b6f8
NSTCG <nithinsteven32@gmail.com>
2022-01-25 22:44:39 +0530

    vertical_jump_added_(infinite_jumping_not_fixed)

--- Assets/Player_controller.cs
+++ Assets/Player_controller.cs
@@ -1,61 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player_controller : MonoBehaviour
 {
+
+    public float speed_walk;
+    public float speed_run;
+    public float jump_intensity;
+    public bool is_running;
+    public bool hasExitTime;
+
+    public static bool Float_to_bool(float num)
+    {
+        return Mathf.Approximately(Mathf.Min(num, 1), 1);
+    }
+
+
     public Animator animator;
+    private Rigidbody2D body;
+
+
+
+    private void Awake()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal"); 
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float jump = Input.GetAxisRaw("Vertical");                     // 1 -1 0
+
+        Input.GetKeyDown(KeyCode.Space);
+
+
+        //changing parameters when running 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed = 2*speed;
+            horizontal = 2 * horizontal;
+            is_running = true;
 
         }
+        is_running = false;
 
-        Debug.Log(speed);
+
+        //apply movement
+        //playing animation
+
+        move_player(horizontal,jump, is_running);
+        player_animation(horizontal, jump);
 
 
-        animator.SetFloat("speed", Mathf.Abs(speed));
+        //how to know 
+
+       // Debug.Log("");
+
+
+
+
+    }
+
+
+
+
+    public void  move_player(float horizonal,float jump,bool is_running)
+    {
+        Vector3 position = transform.position;
+        position.x+=horizonal* ((is_running)?speed_run:speed_walk) *Time.deltaTime;     //1 * 2 * 1
+        transform.position = position;
+
+
+        if (jump > 0)
+        {
+            body.AddForce(new Vector2(0f, jump_intensity), ForceMode2D.Force);
+
+        }
+    }
+
+
+
+
+    public void player_animation(float horizontal, float jump )
+    {
+
+        
+
+        Debug.Log(horizontal);
+
+
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
         Vector3 scale = transform.localScale;
 
 
 
-        if (speed < 0)
+
+
+        if (horizontal < 0)
         {
-            scale.x = -1f * Mathf.Abs(scale.x);     
+            scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
-        {       
+
+        else if (horizontal > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+
         
-        float jump= Input.GetAxisRaw("Jump");  // 1 then 0
 
 
+        animator.SetBool((jump < 0) ? "is_crouching" : "jump", true);
 
-        if (Input.GetKey(KeyCode.Space))
-        {
-            animator.SetBool("jump",true);         
-        }
-        if (!Input.GetKey(KeyCode.Space))
+        if (jump == 0)
         {
             animator.SetBool("jump", false);
-        }
-
-
-
-
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            animator.SetBool("is_crouching", true);
-        }
-        if (!Input.GetKey(KeyCode.C))
-        {
             animator.SetBool("is_crouching", false);
         }
+
 
 
 
@@ -81,11 +149,6 @@ public class Player_controller : MonoBehaviour
         {
             animator.SetBool("attack", false);
         }
-
-
-
-
-
 
     }
 }
